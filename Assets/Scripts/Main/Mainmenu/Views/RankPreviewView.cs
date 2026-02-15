322defdafd36923fdb939b59db87b63b9f7c54e2
Rangga Mulia Tohpati <numberman3250@gmail.com>
2026-02-15 13:57:25 +0700

    feat: fixing some bug

--- Assets/Scripts/Main/Mainmenu/Views/RankPreviewView.cs
+++ Assets/Scripts/Main/Mainmenu/Views/RankPreviewView.cs
@@ -7,10 +7,26 @@ namespace Main.Mainmenu
     public class RankPreviewView : View
     {
         public TMP_Text rankView;
+        public TMP_Text subRankView;
 
         public void UpdatePreview(int rank)
         {
             rankView.text = rank.ToString();
+
+            if(rank == 1)
+            {
+                subRankView.text = "st";
+            }else if(rank == 2)
+            {
+                subRankView.text = "nd";
+            }else if(rank == 3)
+            {
+                subRankView.text = "rd";
+            }
+            else
+            {
+                subRankView.text = "th";
+            }
         }
     }
 }
