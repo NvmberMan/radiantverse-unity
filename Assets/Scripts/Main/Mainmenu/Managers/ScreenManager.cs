7668c0fc85eaf63bf09d33faf41f3fc816a274f2
Rangga Mulia Tohpati <numberman3250@gmail.com>
2026-02-22 08:58:28 +0700

    fix: fixing double jump ai

--- Assets/Scripts/Main/Mainmenu/Managers/ScreenManager.cs
+++ Assets/Scripts/Main/Mainmenu/Managers/ScreenManager.cs
@@ -2,17 +2,15 @@ using UnityEngine;
 
 public class ScreenManager : MonoBehaviour
 {
-    void Start()
+    void Awake()
     {
-        // 1. Matikan orientasi Portrait (Berdiri)
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+        Screen.orientation = ScreenOrientation.AutoRotation;
+
         Screen.autorotateToPortrait = false;
         Screen.autorotateToPortraitUpsideDown = false;
-
-        // 2. Aktifkan kedua sisi Landscape (Tidur)
         Screen.autorotateToLandscapeLeft = true;
         Screen.autorotateToLandscapeRight = true;
-
-        // 3. Set mode ke AutoRotation agar sistem mendeteksi sensor
-        Screen.orientation = ScreenOrientation.AutoRotation;
     }
 }
